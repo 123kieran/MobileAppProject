7170bd0f87702ad8c992460f8b429bef7de2e08e
123kieran <123kieran@gmail.com>
2018-11-09 15:52:49 +0000

    fixed player, added couple of platforms

---
+++ Assets/_scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	Rigidbody2D rigid2D;
+
+	//player variables
+	public float moveSpeed = 6;
+	public float maxSpeed = 2f;
+	public float jumpHeight = 15;
+
+	// grounded variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whereIsGround;
+	private bool grounded;
+
+	private bool doubleJumped;
+
+	// Use this for initialization
+	void Start () {
+		rigid2D = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whereIsGround);
+
+		Move (Input.GetAxis ("Horizontal"));
+		if (grounded)
+			doubleJumped = false;
+
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+			Jump ();
+			//
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && !grounded && !doubleJumped) {
+			doubleJumped = true;
+			Jump();
+			//
+		}
+	}
+
+	// Move player
+	public void Move (float horizontalInput) {
+		Vector2 moveVel = rigid2D.velocity;
+		moveVel.x = horizontalInput * moveSpeed;
+		rigid2D.velocity = moveVel;
+	}
+
+	// Jump 
+	public void Jump () {
+		rigid2D.velocity = new Vector2 (moveSpeed, jumpHeight);
+		
+
+	}
+
+}
