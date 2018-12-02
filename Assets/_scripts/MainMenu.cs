4b1d06ca9581528893cadee9c6597a322df199a3
123kieran <123kieran@gmail.com>
2018-12-02 14:06:02 +0000

    Main menu working

---
+++ Assets/_scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    public string beginLevel;
+    
+
+    // New Game
+    public void NewGame()
+    {
+        // Load first level
+        SceneManager.LoadScene(beginLevel);
+    }
+ 
+    // exit Game
+    public void ExitGame()
+    {
+        // Quit Game
+        //SceneManager.LoadScene(beginLevel);
+        Application.Quit();
+        Debug.Log("User has exited game");
+    }
+
+}
